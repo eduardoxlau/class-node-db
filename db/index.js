3e90817c677a22c18dd699e736274fcccae2a00d
Rafael Sanchez <sanchezpazrafael@gmail.com>
2021-08-21 11:21:13 -0500

    make query

--- db/index.js
+++ db/index.js
@@ -1,10 +1,25 @@
-const users = [
-  {
-    name: "Rafa",
-  },
-  {
-    name: "Henry",
-  },
-];
+require("dotenv").config();
 
-module.exports = users;
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: process.env.HOST_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
+  database: process.env.DB_DB,
+});
+
+connection.connect();
+
+const query1 = () => {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT SUM(valornumerico) as valor, ficha,linea,codficha FROM detallesfichasmunicipales WHERE linea = 9 GROUP BY ficha",
+      function (error, results, fields) {
+        if (error) reject(error);
+        resolve(results);
+      }
+    );
+  });
+};
+
+module.exports = { query1 };
